8bd39e7953faf24dd368adbd586e866cbebfa804
Steve Ngo <stevehngo@hotmail.com>
2021-07-30 01:50:30 -0400

    update

--- app1.js
+++ app1.js
@@ -1,7 +1,11 @@
 let user = prompt('Are you a member of the Glactic Empire?')
 
-if(user.toLowerCase() == 'no'){
-    document.write('<h3>Welcome Hero</h3>')
-} else {
-    document.write('<h4>Self Destruct in 5 Seconds</h4>')
+function getAnswer(){
+    if(user.toLowerCase() == 'no'){
+        document.write('<h3>Welcome Hero</h3>')
+    } else {
+        document.write('<h4>Self Destruct in 5 Seconds</h4>')
+    }
 }
+
+getAnswer();
