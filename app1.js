108d1c19910b2c7f46ea291d2ce7ad6e8d59d8f2
Steve Ngo <stevehngo@hotmail.com>
2021-07-30 02:04:03 -0400

    update

--- app1.js
+++ app1.js
@@ -4,6 +4,8 @@ function getAnswer(){
     if(user.toLowerCase() == 'no'){
         document.write('<h3>Welcome Hero</h3>')
     } else {
+        document.write('<h4>These are not the droids you are looking for </h4>')
+    } else if {
         document.write('<h4>Self Destruct in 5 Seconds</h4>')
     }
 }
